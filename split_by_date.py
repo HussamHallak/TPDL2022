bc131c92743b315472fec8e6de484ffcc181213e
Hussam Hallak <hhallak@cs.odu.edu>
2023-02-03 18:55:35 -0500

    Add files via upload

--- split_by_date.py
+++ split_by_date.py
@@ -6,6 +6,7 @@ Created on Sat Jul  2 08:19:52 2022
 """
 import os
 import sys
+import time
 from newspaper import Article   
 import requests
 import json
@@ -22,32 +23,41 @@ def split(links):
         os.makedirs("output_redo")
 
     for link in links:
-        response = requests.get(link)
-        if response.status_code == 200:
-            final_link = link
-        else:
-            final_link = response.url 
-
         try:
-            story = Article(final_link)
-            story.download()
-            story.parse()
-            date_time = str(story.publish_date)
-            split_date = date_time.split()  
-            date = split_date[0]
-            # article = {"url": link, "final_url": final_link, "publish_date": date, "title": story.title, "text": story.text}
-            # with open("output/" + date + ".json", "a", encoding = 'utf-8') as output_file:
-            #     json.dump(article, output_file)
-            #     output_file.write("\n")
-            with open("output/" + date + ".txt", "a", encoding = 'utf-8') as output_file:
-                output_file.write(link + "\n")
+            with requests.get(link, stream=True) as response:
+                response = requests.get(link)
+                if response.status_code == 200:
+                    final_link = link
+                else:
+                    final_link = response.url 
+
+                try:
+                    story = Article(final_link)
+                    story.download()
+                    story.parse()
+                    date_time = str(story.publish_date)
+                    split_date = date_time.split()  
+                    date = split_date[0]
+                    # article = {"url": link, "final_url": final_link, "publish_date": date, "title": story.title, "text": story.text}
+                    # with open("output/" + date + ".json", "a", encoding = 'utf-8') as output_file:
+                    #     json.dump(article, output_file)
+                    #     output_file.write("\n")
+                    with open("output/" + date + ".txt", "a", encoding = 'utf-8') as output_file:
+                        output_file.write(link + "\n")
+                except:
+                    print("The script was not able to extract published date. Moving the url to be crawled later.")
+                    print("link: ", link)
+                    print("status code: ", response.status_code)
+                    print("final link: ", final_link)
+                    with open("output_redo/" + "links_to_redo" + ".txt", "a", encoding = 'utf-8') as output_redo:
+                            output_redo.write(link + "\n")            
+                continue
         except:
-            print("The script was not able to extract published date. Moving the url to be crawled later.")
-            print("link: ", link)
-            print("status code: ", response.status_code)
-            print("final link: ", final_link)
-            with open("output_redo/" + "links_to_redo" + ".txt", "a", encoding = 'utf-8') as output_redo:
-                    output_redo.write(link + "\n")            
+            print("Connection refused by the server..")
+            print("Sleeping for 5 seconds")
+            print("ZZzzzz...")
+            time.sleep(5)
+            print("It's time to continue...")
         continue
 
 if __name__ == "__main__":
